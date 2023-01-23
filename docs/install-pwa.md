612a3fe74fe5db228917e8fcfe5e10233f908b65
Anton Heitz <anton.j.heitz@gmail.com>
2023-01-23 22:59:43 +0100

    0.0.0

---
+++ docs/install-pwa.md
@@ -0,0 +1,19 @@
+# Install the App on your device
+
+Installing the Progressive Webapp on your device is as simple as it can be. 
+
+## Android, Windows, Linux, MacOS
+
+The only needed base software is Chrome. 
+
+1. Open Chrome
+2. Navigate to your URL deployed to to in [this Guide](publish-pwa.md) or use the official main deployment:
+`https://antonheitz.github.io/targetnotes/`
+3. Click on the `Install targetnotes` button in the Adress Bar
+
+## IOS
+
+1. Open Safari
+2. Navigate to your URL deployed to to in [this Guide](publish-pwa.md) or use the official main deployment:
+`https://antonheitz.github.io/targetnotes/`
+3. klick on `share` and then on `add to home screen`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule,
+    // angular material
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
