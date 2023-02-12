702b60f7252e398749ca7814507b20110a319eef
Anton Heitz <anton.j.heitz@gmail.com>
2023-02-13 00:16:15 +0100

    optimize width calculation

--- src/app/view/main/main.component.ts
+++ src/app/view/main/main.component.ts
@@ -19,18 +19,16 @@ export class MainComponent {
   }
 
   currentResizing: HTMLElement | null = null;
-  width: number = 0;
-  original_x: number = 0;
+  baseWidth: number = 0;
 
   startResize(event: MouseEvent): void {
     this.currentResizing = ((event.target as HTMLElement).parentElement as HTMLElement);
-    this.width = this.currentResizing.clientWidth;
-    this.original_x = event.clientX;
+    this.baseWidth = this.currentResizing.clientWidth - event.clientX;
   }
 
   resize(event: MouseEvent): void {
     if (this.currentResizing !== null) {
-      this.currentResizing.style.width = `${(this.width + event.clientX - this.original_x)}px`;
+      this.currentResizing.style.width = `${(this.baseWidth + event.clientX)}px`;
     }
   }
 
