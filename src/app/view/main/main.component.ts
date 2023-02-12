ec60395c2e2fe9ef4c44874c3ed02bd70db29fd2
Anton Heitz <anton.j.heitz@gmail.com>
2023-02-13 00:13:36 +0100

    resizing done

--- src/app/view/main/main.component.ts
+++ src/app/view/main/main.component.ts
@@ -17,4 +17,26 @@ export class MainComponent {
   get currentMainView(): CurrentMainView {
     return this.stateService.getCurrentMainView();
   }
+
+  currentResizing: HTMLElement | null = null;
+  width: number = 0;
+  original_x: number = 0;
+
+  startResize(event: MouseEvent): void {
+    this.currentResizing = ((event.target as HTMLElement).parentElement as HTMLElement);
+    this.width = this.currentResizing.clientWidth;
+    this.original_x = event.clientX;
+  }
+
+  resize(event: MouseEvent): void {
+    if (this.currentResizing !== null) {
+      this.currentResizing.style.width = `${(this.width + event.clientX - this.original_x)}px`;
+    }
+  }
+
+  endResize(event: MouseEvent): void {
+    if (this.currentResizing !== null) {
+      this.currentResizing = null;
+    }
+  }
 }
